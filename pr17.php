e8102f06a66d46506f684712f77f79c10e6c640c
Shahnuma Bano <shahnumashahnumabano4@gmail.com>
2025-08-30 08:31:44 +0530

    Add files via upload

---
+++ pr17.php
@@ -0,0 +1,10 @@
+<?php
+//WAP to print odd number in between 20-80 using php.
+for ($i = 20; $i <=80; $i++){
+    if($i % 2 !=0){
+        echo"$i";
+        echo"<br>";
+    }
+}
+
+?>
---
+++ pr18.php
@@ -0,0 +1,9 @@
+<?php
+// 1 se 100 ke beech even numbers.
+for($i=1; $i<=100; $i++){
+    if($i % 2 == 0){
+        echo"$i";
+        echo"<br>";
+    }
+}
+?>
